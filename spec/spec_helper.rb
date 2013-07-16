176202dd0c582bce8c8ca201f3766d0f97b45dc6
Stephan Schubert <stephan@frozencherry.de>
2013-07-16 14:31:26 +0200

    Bugfix - Do not use the RSpec::Matchers namespace as this can lead to loading issues (e.g. the real RSpec::Matchers was not loaded afterwards) and it's bad style anyway. Use your own namespace and include your custom matchers in the RSpec.configure block in your spec/spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,14 @@ SimpleCov.start do
   add_filter '/vat_id_validator/version'
 end
 
+require 'vat_id_validator'
+require 'support/active_model_test_class'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.include VatIdValidator::Matchers
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
@@ -15,6 +19,3 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 end
-
-require 'vat_id_validator'
-require 'support/active_model_test_class'
