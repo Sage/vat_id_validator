6543c88a1166bbc2598d990a0a042c0e05f06aed
LeFnord <peter.scholz@sage.com>
2016-10-19 13:24:52 +0200

    make it rails 4,5 ready ;)

--- spec/support/active_model_test_class.rb
+++ spec/support/active_model_test_class.rb
@@ -1,7 +1,10 @@
+require 'active_support'
+require 'active_support/core_ext/module/delegation'
 require 'active_model/naming'
 require 'active_model/errors'
 require 'active_model/translation'
 require 'active_model/validations'
+require 'active_model/callbacks'
 
 class ActiveModelTestClass
   extend ActiveModel::Naming
